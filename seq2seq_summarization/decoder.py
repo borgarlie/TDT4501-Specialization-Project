77ed75aa50dd4823a686e3e880ee70ea6f6ebd80
Borgar Lie <borgar.lie@gmail.com>
2017-10-03 13:54:44 +0200

    Fix GRU decoder for multiple layers

--- seq2seq_summarization/decoder.py
+++ seq2seq_summarization/decoder.py
@@ -32,15 +32,13 @@ class DecoderRNN(nn.Module):
         self.hidden_size = hidden_size
 
         self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
         self.out = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax()
 
     def forward(self, input, hidden, batch_size=1):
         output = self.embedding(input).view(1, batch_size, self.hidden_size)
-        for i in range(self.n_layers):
-            output = F.relu(output)
-            output, hidden = self.gru(output, hidden)
+        output, hidden = self.gru(output, hidden)
         output = self.softmax(self.out(output[0]))
         return output, hidden
 
@@ -66,7 +64,7 @@ class AttnDecoderRNN(nn.Module):
         self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
         self.dropout = nn.Dropout(self.dropout_p)
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size, self.n_layers)
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, input, hidden, encoder_outputs, batch_size=1):
@@ -81,9 +79,7 @@ class AttnDecoderRNN(nn.Module):
         output = torch.cat((embedded[0], attn_applied.squeeze(1)), 1)
         output = self.attn_combine(output).unsqueeze(0)
 
-        for i in range(self.n_layers):
-            output = F.relu(output)
-            output, hidden = self.gru(output, hidden)
+        output, hidden = self.gru(output, hidden)
 
         output = F.log_softmax(self.out(output[0]))
         return output, hidden, attn_weights
